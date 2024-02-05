0735fdd64e8cde3ffe1fb91fe66c7f5b7b57c2b9
Mehmet Eren <merengormez@outlook.com>
2024-02-05 20:36:18 +0300

    IdentityServerUrl yapılandırması eklendi ve Ocelot entegrasyonu yapıldı

--- ApiGateways/Alpata.ApiGateway/Alpata.Gateway/Program.cs
+++ ApiGateways/Alpata.ApiGateway/Alpata.Gateway/Program.cs
@@ -1,6 +1,20 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddOcelot();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+
+app.UseDeveloperExceptionPage();
+
+app.MapControllers();
+
+await app.UseOcelot();
 
 app.Run();
