a06782a51aa9586c83a501dc72cda91fe49d36d0
EroNWs <merengrmz@gmail.com>
2024-02-06 15:00:04 +0300

    Coded interface structure for Meeting to facilitate data access abstraction and improve maintainability.

---
+++ Services/Meeting.Microservice/Meeting.Business/Interfaces/Services/IMeetingService.cs
@@ -0,0 +1,15 @@
+﻿using Meeting.Core.Utilities.Results.Interfaces;
+using Meeting.Dtos.MeetingCrudDtos;
+
+namespace Meeting.Business.Interfaces.Services;
+
+public interface IMeetingService
+{
+    Task<IDataResult<MeetingListDto>> GetByIdAsync(Guid id);
+    Task<IDataResult<List<MeetingListDto>>> GetAllAsync();
+    Task<IDataResult<MeetingCreateDto>> AddAsync(MeetingCreateDto meetingCreateDto);
+    Task<IDataResult<MeetingUpdateDto>> UpdateAsync(MeetingUpdateDto meetingUpdateDto);
+    Task<IResult> DeleteAsync(Guid id);
+
+
+}
