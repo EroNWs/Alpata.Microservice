2d5ddc7bd655db4fd662c374fa0b0ed4828a122c
EroNWs <merengrmz@gmail.com>
2024-02-06 14:47:51 +0300

    Implemented Dependency Injection setup for EF Core to enhance modularity and testability of the application.

---
+++ Services/Meeting.Microservice/Meeting.Dal.EF/Extensions/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Meeting.Dal.EF.Repositories;
+using Meeting.Dal.EF.Seeds;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Meeting.Dal.EF.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddEFCoreServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IMeetingRepository, MeetingRepository>();
+
+        MeetingPlanningSeed.SeedAsync(configuration).GetAwaiter().GetResult();
+
+        return services;
+    }
+
+
+
+
+}
